3f810e28eaadc4cbcd5bb9c83e1b5484d0a1338e
Michael Ratliff <email@michaelratliff.com>
2013-12-06 19:29:13 -0500

    Fix tmux status on linux

--- bin/tmux-status.sh
+++ bin/tmux-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -d ~/code/min ]]; then
-	echo $(cd ~/code/min; git branch | grep '*' | tr -d ' ') $(date +"%F %I:%M %p") [$(uptime | cut -d : -f 4 | tr -d ',' | cut -c 2-)]
+	echo $(cd ~/code/min; git branch | grep '*' | tr -d ' ') $(date +"%F %I:%M %p") [$(uptime | sed -e 's/.*average: //' -e ' s/,//g')]
 else
-	echo $(date +"%F %I:%M %p") [$(uptime | cut -d : -f 4 | tr -d ',' | cut -c 2-)]
+	echo $(date +"%F %I:%M %p") [$(uptime | sed -e 's/.*average: //' -e 's/,//g')]
 fi
