e0f3e64c6c715ad0dd255bc2bdf32aa66ded194c
Michael Ratliff <email@michaelratliff.com>
2014-03-04 14:40:11 -0500

    Add Irssi config

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc bash_aliases bash_functions vimrc vim tmux.conf"    # list of files/folders to symlink in homedir
+files="bashrc bash_aliases bash_functions vimrc vim tmux.conf irssi"    # list of files/folders to symlink in homedir
 mac_files="tmux.osx.conf"
 
 ##########
--- refresh.sh
+++ refresh.sh
@@ -2,5 +2,7 @@
 
 git pull && git submodule init && git submodule update
 
+[[ -d ~/.irssi ]] || ln -s ~/dotfiles/irssi ~/.irssi
+
 //wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -O bin/git-completion.bash
 
