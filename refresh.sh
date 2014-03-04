892f2028bc603fe0912f2c50c419d225e74e6e62
Michael Ratliff <email@michaelratliff.com>
2014-03-04 14:43:12 -0500

    Fix refesh script

--- refresh.sh
+++ refresh.sh
@@ -4,5 +4,5 @@ git pull && git submodule init && git submodule update
 
 [[ -d ~/.irssi ]] || ln -s ~/dotfiles/irssi ~/.irssi
 
-//wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -O bin/git-completion.bash
+#wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -O bin/git-completion.bash
 
