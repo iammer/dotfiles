54bf64d0dff5da66ed9a92d13ec0024ba1941c11
Michael Ratliff <email@michaelratliff.com>
2014-07-01 14:02:45 -0400

    Make tmux work with bash4 on OSX

--- bin/startTerm.sh
+++ bin/startTerm.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
+[[ -x /usr/local/bin/bash ]] && SHELL=/usr/local/bin/bash
 
-/usr/local/bin/tmux attach || /usr/local/bin/tmux new
+/usr/local/bin/tmux attach || /usr/local/bin/tmux new $SHELL
