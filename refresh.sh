ad4c21ed364b86ecbc264aacf054b2fe329a14b4
Michael Ratliff <email@michaelratliff.com>
2015-05-15 15:38:12 -0400

    Fix conflict in refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -4,5 +4,5 @@ git pull && git submodule init && git submodule update
 
 [[ -e ~/.gitignore_global ]] || ln -s ~/dotfiles/gitignore_global ~/.gitignore_global 
 
-[[ -e ~/bin/git-completion.bash ]] || wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -O ~/bin/git-completion.bash
+[[ -e ~/dotfiles/bin/git-completion.bash ]] || wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -O ~/dotfiles/bin/git-completion.bash
 
