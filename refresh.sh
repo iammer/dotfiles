0ef996ee755809294d66c75aa29356d8c78ef163
Michael Ratliff <email@michaelratliff.com>
2015-10-26 15:30:58 -0400

    Add Desk

--- refresh.sh
+++ refresh.sh
@@ -4,5 +4,7 @@ git pull && git submodule init && git submodule update
 
 [[ -e ~/.gitignore_global ]] || ln -s ~/dotfiles/gitignore_global ~/.gitignore_global 
 
+[[ -L ~/.desk ]] || ln -s ~/dotfiles/desk ~/.desk  
+
 [[ -e ~/dotfiles/bin/git-completion.bash ]] || wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -O ~/dotfiles/bin/git-completion.bash
 
