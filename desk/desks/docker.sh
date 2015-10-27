8a5cc5ba98bc841ed0ca1a85d752a5df23a284ea
Michael Ratliff <email@michaelratliff.com>
2015-10-27 14:14:44 -0400

    Move desk around

---
+++ desk/desks/docker.sh
@@ -0,0 +1,12 @@
+# docker.sh
+#
+# Description: Commands to help work with docker instances
+#
+
+# alias for docker-machine
+alias dm='docker-machine'
+
+# setup environment for docker-maching
+alias setupDocker='eval "$(docker-machine env dev)"'
+
+
