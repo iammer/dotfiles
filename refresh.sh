8689c33d0bfcd09c0b564d4e692d99fe27c6fa39
Michael Ratliff <email@michaelratliff.com>
2015-11-25 14:00:14 -0500

    Add agignore and change the way refresh checks for missing links

--- refresh.sh
+++ refresh.sh
@@ -4,9 +4,10 @@ git pull && git submodule init && git submodule update
 
 [[ -e ~/.gitignore_global ]] || ln -s ~/dotfiles/gitignore_global ~/.gitignore_global 
 
-[[ -L ~/.desk ]] || ln -s ~/dotfiles/desk ~/.desk  
-
-[[ -L ~/.ctags ]] || ln -s ~/dotfiles/ctags ~/.ctags
+files="desk ctags agignore"
+for file in $files; do
+	[[ -L ~/.$file ]] || ln -s ~/dotfiles/$file ~/.$file  
+done
 
 [[ -e ~/dotfiles/bin/git-completion.bash ]] || wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -O ~/dotfiles/bin/git-completion.bash
 
