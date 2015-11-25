12d7ce7522727c4cdec719bcabfcc176ff114f2f
Michael Ratliff <email@michaelratliff.com>
2015-11-25 12:03:00 -0500

    Add ctags files

--- refresh.sh
+++ refresh.sh
@@ -6,5 +6,7 @@ git pull && git submodule init && git submodule update
 
 [[ -L ~/.desk ]] || ln -s ~/dotfiles/desk ~/.desk  
 
+[[ -L ~/.ctags ]] || ln -s ~/dotfiles/ctags ~/.ctags
+
 [[ -e ~/dotfiles/bin/git-completion.bash ]] || wget https://github.com/git/git/raw/master/contrib/completion/git-completion.bash -O ~/dotfiles/bin/git-completion.bash
 
