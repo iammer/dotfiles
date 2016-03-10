3eec6271db3984ba4c961623f976a5758bb8c849
Michael Ratliff <email@michaelratliff.com>
2016-03-10 12:45:36 -0500

    Don't show current branch in tmux status line

--- bin/tmux-status.sh
+++ bin/tmux-status.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if [[ -d ~/code/min ]]; then
-	echo $(cd ~/code/min; git branch | grep '*' | tr -d ' ') $(date +"%F %I:%M %p") [$(uptime | sed -e 's/.*averages*: //' -e ' s/,//g')]
-else
-	echo $(date +"%F %I:%M %p") [$(uptime | sed -e 's/.*averages*: //' -e 's/,//g')]
-fi
+echo $(date +"%F %I:%M %p") [$(uptime | sed -e 's/.*averages*: //' -e 's/,//g')]
