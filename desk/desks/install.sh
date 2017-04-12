781e2b1e40d664fff2b3859342ecc33b973e5416
Michael Ratliff <email@michaelratliff.com>
2017-04-12 12:24:01 -0400

    Update install desk

--- desk/desks/install.sh
+++ desk/desks/install.sh
@@ -8,17 +8,30 @@ alias brew='/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Hom
 # Necessary OSX things
 alias brewBase='brew install ack bash coreutils entr findutils gawk git git-lfs gnu-sed gnu-tar gnutls grep jq mobile-shell neovim nmap openssl parallel perl readline tmux unzip vim w3m wget watchman xz zbackup the_silver_searcher'
 
-# Necessary Linux things (use sudo)
+# Necessary Linux things (uses sudo)
 debBase() {
-	apt-get update
-	apt-get -y install git tmux curl build-essentials vim silversearch-ag entr jq nmap parallel watchman xz zbackup openssl
+	sudo apt-get update
+	sudo apt-get -y install git tmux curl build-essentials vim silversearch-ag entr jq nmap parallel watchman xz zbackup openssl
+}
+
+#Neovim (uses sudo)
+neovim() {
+	sudo apt-get -y install software-properties-common
+	sudo add-apt-repository ppa:neovim-ppa/stable
+	sudo apt-get update
+	sudo apt-get -y install neovim
 }
 
 # Install NVM
 alias nvm='curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash'
 
 # Install Rust
-alias rust='curl https://sh.rustup.rs -sSf | sh'
+rust() {
+	curl https://sh.rustup.rs -sSf -o /tmp/rust-init &&
+	chmod a+x /tmp/rust-init &&
+	/tmp/rust-init -y --no-modify-path --default-toolchain nightly &&
+	rm /tmp/rus-init
+}
 
 # Install SDKMan
 alias sdkman='curl -s "https://get.sdkman.io" | bash'
